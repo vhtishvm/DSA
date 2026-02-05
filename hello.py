9c96d1ee767f68f07ff7ac6056bfe800bc29055b
vhtishvm <vhtishvm@gmail.com>
2026-02-05 02:33:39 +0000

    Intial commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+a = int(input("Enter the first nubmer: "))
+b = int(input("Etner the second number: "))
+
+for i in  range(a,b):
+    print(i)
+
+
+print("Now for the while loop::::   ")
+
+n = a   
+while n<=b:
+    print(n)
+    n += 1
