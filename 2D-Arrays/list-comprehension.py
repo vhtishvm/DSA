4cd78dcf08b2506da480c5ef8334267d5db9ba85
vhtishvm <vhtishvm@gmail.com>
2026-02-12 16:13:46 +0000

    Unexpected bug

--- 2D-Arrays/list-comprehension.py
+++ 2D-Arrays/list-comprehension.py
@@ -1,3 +1,5 @@
+import random
+
 n = [12,23,34,45,56]
 s = []
 
@@ -10,3 +12,13 @@ print(s)
 #Finally learnt list comprehension
 x = [x*2 for x in range(6)]
 print(x)
+
+#Create a list of numbers from 1–10 but add +1 to each number
+y = [a+1 for a in range(1,11)]
+
+print(y)
+
+t = []
+
+u = [t.append(v+random.random()) for v in range(15)]
+print(u)
