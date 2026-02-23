19a4192aac283f2a55a92145671f58ffd1a109ec
vhtishvm <vhtishvm@gmail.com>
2026-02-23 15:38:30 +0000

    Selection sort

--- sorting/01-solution.py
+++ sorting/01-solution.py
@@ -1,18 +1,19 @@
 '''My version of selection sort'''
 
-# array = [12,43,23,65,34,13,16,25,2,74,78,75,43,38,5,17]
-array = [1,2,3,4,5,6,7]
+array = [12,43,23,65,34,13,16,25,2,74,78,75,43,38,5,17]
+
 
 
 for i in range(len(array)):
     minimum = array[i]
-    loc = i
+    r = i
+
     for j in range(i,len(array)):
-        if array[j] < minimum:
+        if minimum > array[j]:
             minimum = array[j]
-            loc = j
-    print(f"The array so far: {array}")
-    array[loc] = array[i]
+            r = j
+    
+    array[r] = array[i]
     array[i] = minimum
 
 print(array)
