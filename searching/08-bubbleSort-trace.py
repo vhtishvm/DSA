e9296e06abc2651b4e400dc8574888205937b313
vhtishvm <vhtishvm@gmail.com>
2026-02-24 12:23:19 +0000

    Bubble Sort

---
+++ searching/08-bubbleSort-trace.py
@@ -0,0 +1,16 @@
+size = int(input())
+array = list(map(int, input().split()))
+
+for i in range(len(array)-1):
+    sw = 0
+    for j in range(len(array)-1-i):
+        if array[j] > array[j+1]:
+            array[j], array[j+1] = array[j+1],array[j]
+            sw +=1
+        
+
+    print(f"Pass {i+1}: {' '.join(map(str,array))} , swaps = {sw}")
+    if sw == 0:
+        break
+    
+
